337169055c65f85563eacdf4b0c35eae57bd1d50
cporrier <carole.porrier@gmail.com>
2017-06-25 20:23:41 -0400

    Update mergesort.py
    Ajout description des fonctions

--- mergesort.py
+++ mergesort.py
@@ -5,6 +5,9 @@
 # Python 3
 
 def merge(A,B):
+	"""
+	Fonction qui prend en entrée deux listes A et B déjà triées et qui les fusionne en une seule liste triée
+	"""
 	n = len(A) + len(B)
 	i = 0
 	j = 0
@@ -19,6 +22,9 @@ def merge(A,B):
 	return C
 
 def tri(T):
+	"""
+	Fonction récursive permettant de trier une liste T, en utilisant la fonction merge
+	"""
 	n = len(T)
 	if n <= 1 : return T
 	elif n == 2 : return (min(T),max(T))
